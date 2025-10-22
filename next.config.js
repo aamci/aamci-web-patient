1735728babd4ec7a8100ac1bf628c65761dba011
Marvel Lawson <lawsonmarvel@gmail.com>
2025-10-23 00:06:01 +0200

    fix issue with port

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'standalone', // <- add this
   env: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || '',
+    NEXT_PUBLIC_API_BASE_URL: 'https://api-ieis.onrender.com',
   },
   experimental: { typedRoutes: true },
 };
