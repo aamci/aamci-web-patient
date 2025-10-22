95e66f7a988320f15cb7675ad9ea4219148d1846
Marvel Lawson <lawsonmarvel@gmail.com>
2025-10-22 23:47:28 +0200

    fix issue with port

--- app/health/page.tsx
+++ app/health/page.tsx
@@ -5,6 +5,7 @@ export default function HealthPage() {
     <div style={{ padding: 24 }}>
       <h2>✅ Web-Patient Front is Healthy</h2>
       <p>Environment: {process.env.NODE_ENV}</p>
+      <p>NEXT_PUBLIC_API_BASE_URL: {process.env.NEXT_PUBLIC_API_BASE_URL}</p>
       <p>Timestamp: {new Date().toLocaleString()}</p>
     </div>
   );
