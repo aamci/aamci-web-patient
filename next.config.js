7cb59daa8b4faf940ef0a876854e2ca37905b9a3
Marvel Lawson <lawsonmarvel@gmail.com>
2025-10-23 00:00:30 +0200

    fix issue with port

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone', // <- add this
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || '',
+  },
   experimental: { typedRoutes: true },
 };
 module.exports = nextConfig;
