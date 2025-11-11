4d31a8da6ade88587562fa2d9bd922e5cd6c77a8
Marvel Lawson <lawsonmarvel@gmail.com>
2025-11-11 23:54:18 +0100

    mise a jour

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone', // <- add this
-  // env: {
-  //   NEXT_PUBLIC_API_BASE_URL: 'https://api-ieis.onrender.com',
-  // },
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: 'https://api-ieis.onrender.com',
+  },
   experimental: { typedRoutes: true },
 };
 module.exports = nextConfig;
